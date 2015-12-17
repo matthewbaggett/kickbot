0060691c4a0094e8b0b0b0e5e08eac10b16a35aa
Matthew Baggett <matthew@baggett.me>
2015-12-17 15:38:43 +0000

    Adding YAML/Kint/Telegram API

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
 require_once("../vendor/autoload.php");
+
+use Symfony\Component\Yaml\Yaml;
+
+$config = Yaml::parse(file_get_contents('../config.yml'));
+
+\Kint::dump($config);
+
+$app = new \Slim\App();
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $url = $this->router->pathFor('hello', ['name' => 'Josh']);
+
+    return $response;
+})->setName('hello');
